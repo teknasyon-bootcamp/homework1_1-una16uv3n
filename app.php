1f0e7f1af89a821b26b3f7a8ce0a4f1e82a23b18
Ünal Güven <sunalgvn@gmail.com>
2021-08-17 23:17:54 +0300

    1. Ödev

--- app.php
+++ app.php
@@ -18,3 +18,15 @@ $age = 25;
  * 
  * İyi çalışmalar.
  */
+ if($age >=0 && $age <=14){
+	echo"Çocuk";
+
+}elseif ($age >=15 && $age <=24) {
+	echo"Genç";
+	
+}elseif($age >=25 && $age <=64){
+	echo"Yetişkin";
+
+}elseif($age >=65){
+	echo"Yaşlı";
+}
